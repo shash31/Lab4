bdaf2dea444e6b8bf3be905d332321109b0cf907
shash123-d <88714649+shash123-d@users.noreply.github.com>
2025-04-30 00:44:09 -0700

    Fixed bugs in lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,7 +7,8 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        if (typeof num1 !== 'number') { return false; }
+        let result = 0;
 
         result = num1 + num2;
 
